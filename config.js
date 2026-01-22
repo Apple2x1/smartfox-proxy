41086fa79305993188c9c232feb534f3ffac1d9d
Apple <waseemchandio170@gmail.com>
2026-01-23 00:14:05 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * File: config.js
+ * Purpose: Original SmartFox server aur proxy settings define karta hai
+ * Repo: https://github.com/<YOUR_USERNAME>/smartfox-proxy
+ */
+
+module.exports = {
+    // Original SmartFox server
+    SMARTFOX_HOST: "live2.chatgum.com",  
+    SMARTFOX_PORT: 9933,                 // SmartFox TCP port
+
+    // Proxy server port (app connect karega)
+    LISTEN_PORT: 9933                    
+};
